b4637817d25a1ee280d03e8147a2ff2a29f00c60
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-11 16:29:48 +0200

    ajout calculs

---
+++ src/exercices/calculs.js
@@ -0,0 +1,32 @@
+console.log(1+1);
+console.log(1-1);
+console.log(1*1);
+console.log(1/1);
+console.log(1%1)
+
+console.log(2950*6/85+(85*9)-60);
+
+let compteur = 0;
+console.log(compteur);
+
+compteur ++;
+console.log(compteur);
+
+compteur +=10;
+console.log(compteur);
+
+compteur --;
+console.log(compteur);
+
+compteur -=5;
+console.log(compteur);
+
+console.log(2 ** 2);
+console.log(2 ** 10);
+
+console.log(Math.pow(2, 10));
+
+console.log(Math.sqrt(16));
+console.log(Math.sqrt(100));
+
+console.log(Math.round(Math.random() * 100));
