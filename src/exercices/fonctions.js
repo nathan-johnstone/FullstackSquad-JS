65cdefcf3f9d6609f4affc2ee0c5c9e61b9c1650
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-12 15:23:40 +0200

    ex fonctions

---
+++ src/exercices/fonctions.js
@@ -0,0 +1,11 @@
+function addition(a = 0){
+    console.log(a + 33);
+};
+
+addition(1);
+
+function additionDeux(a = 0, b = 0){
+    console.log(a + b);
+};
+
+additionDeux(1 + 2)
--- src/exercices/tableaux.js
+++ src/exercices/tableaux.js
@@ -9,3 +9,15 @@ console.log(tabUser)
 console.log(tabUser[2])
 
 console.log(tabUser[2][1])
+
+tabUser.push("test","other",42)
+
+console.log('Après ajout :', tabUser)
+
+console.log('Element supprimé :', tabUser.pop())
+
+console.log('Après suppression :', tabUser)
+
+tabUser.reverse()
+
+console.log('Après reverse :', tabUser)
