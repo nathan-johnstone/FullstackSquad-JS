4f831f9d5257e88eab47e0c72ce10ea7bc2fe06c
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-12 16:12:19 +0200

    modified:   src/exercices/fonctions.js

--- src/exercices/fonctions.js
+++ src/exercices/fonctions.js
@@ -8,4 +8,22 @@ function additionDeux(a = 0, b = 0){
     console.log(a + b);
 };
 
-additionDeux(1 + 2)
+additionDeux(1, 2)
+
+let additionTrois = x => x + 10;
+
+console.log(additionTrois(2))
+
+function additionQuatre(a = 0, b = 2){
+    return a + b;
+};
+
+console.log(additionQuatre(10, 20));
+
+function additionnerOptiSécurisée(firstNumber=0, secondNumber=0) {
+    if (typeof firstNumber !== 'number' || typeof secondNumber !== 'number') {
+        throw new Error('Les paramètres doivent être des nombres');
+    }
+    return firstNumber + secondNumber;
+}
+console.log(additionnerOptiSécurisée("azerty", 3));
