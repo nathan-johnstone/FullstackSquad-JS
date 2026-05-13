ddbdc985540586f985547d98d51b1fb3cbee9feb
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-13 10:13:23 +0200

    modified:   src/exercices/boucles.js

--- src/exercices/boucles.js
+++ src/exercices/boucles.js
@@ -1,12 +1,30 @@
 let notesTibo = [0, 3, 4, 6, 18, 19, 4];
 
 function calculerMoyenne(tab){
-    let moyenne = 0
+    let moyenne = 0;
     for (let i = 0; i < tab.length; i++) {
-        moyenne = tab[i] + moyenne;
+        moyenne += tab[i];
     }
     return moyenne / tab.length;
 }
 
-console.log("La moyenne des notes de Tibo est : ", calculerMoyenne(notesTibo))
+console.log("La moyenne des notes de Tibo est : ", calculerMoyenne(notesTibo));
 
+function calculerMoyenne2(tab){
+    let moyenne = 0;
+    for (const i of tab) {
+        moyenne += i;
+    }
+    return moyenne / tab.length;
+}
+
+console.log(calculerMoyenne2(notesTibo));
+
+let x = 0;
+notesTibo.forEach((i) => x += i);
+
+console.log(x / notesTibo.length);
+
+let notesMap = notesTibo.map((i) => i * 2);
+
+console.log(notesMap);
