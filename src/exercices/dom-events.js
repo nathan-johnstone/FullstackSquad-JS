e52e500d74dd4940c74d4a72c9c88890640fb0b8
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-20 11:56:17 +0200

    Ajout d'une fonctionnalité pour afficher une image à la position du clic dans la section principale

--- src/exercices/dom-events.js
+++ src/exercices/dom-events.js
@@ -11,3 +11,15 @@ const titre = document.querySelector('h2');
 btnAjouter.addEventListener('click', () => titre.classList.add("class"));
 btnSupprimer.addEventListener('click', () => titre.classList.remove("class"));
 btnToggle.addEventListener('click', () => titre.classList.toggle("class"));
+
+document.addEventListener('click', (eventOnClick) => {
+    let xPos = eventOnClick.x;
+    let yPos = eventOnClick.y;
+    const image = document.createElement('img');
+    image.src = "https://picsum.photos/200";
+    document.body.appendChild(image);
+    image.style.display = "block";
+    image.style.position = "absolute";
+    image.style.top = yPos + "px";
+    image.style.left = xPos + "px";
+});
