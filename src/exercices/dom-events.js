ecd2463af692c6075008b0a2ca07c62c7005648e
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-20 09:47:42 +0200

    Mise à jour de la page "DOM Events"

--- src/exercices/dom-events.js
+++ src/exercices/dom-events.js
@@ -2,3 +2,12 @@ const element = document.querySelector('h1');
 const content = document.querySelector('h1').innerText;
 
 element.addEventListener('click', () => {(element.innerText == content) ? element.innerText = "--🥳--" : element.innerText = content});
+
+const btnAjouter = document.querySelector('#ajouter');
+const btnSupprimer = document.querySelector('#supprimer');
+const btnToggle = document.querySelector('#toggle');
+const titre = document.querySelector('h2')
+
+btnAjouter.addEventListener('click', () => titre.classList.add("class"));
+btnSupprimer.addEventListener('click', () => titre.classList.remove("class"));
+btnToggle.addEventListener('click', () => titre.classList.toggle("class"));
