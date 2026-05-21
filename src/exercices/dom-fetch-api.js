1f7b86ba1070c86d090375434f788a3f3c55f538
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-21 16:02:20 +0200

    Ajout de l'attribut type="module"

--- src/exercices/dom-fetch-api.js
+++ src/exercices/dom-fetch-api.js
@@ -53,3 +53,46 @@ const apiNorris = async () => {
 }
 
 document.querySelector('button').addEventListener('click', apiNorris);
+
+
+
+// let getData = '';
+
+// async function apiNorris(){
+//     try {
+//         const rawData = await fetch('https://api.chucknorris.io/jokes/random');
+//         if (!rawData.ok || rawData.status !== 200) {
+//             console.error(rawData.statusText);
+//             return;
+//         }
+//         const data = await rawData.json();
+//         getData = data;
+
+//     } catch (error) {
+//         console.error(error);
+//     }
+// };
+
+// //? await fonctionne car type="module" dans le HTML
+// await apiNorris();
+// console.log(getData);
+
+
+
+// function fetchQuotesThenCatch() {
+//     let mainTitle = document.querySelector('#chuckNorrisFact');
+    
+//     // 1. On lance la requête vers l'API
+//     fetch('https://api.chucknorris.io/jokes/random')
+//         // 2. On convertit la réponse brute en objet JavaScript
+//         .then(response => response.json())
+//         // 3. On utilise les données reçues pour mettre à jour le HTML
+//         .then(dataTransformed => {
+//             console.log(dataTransformed);
+//             mainTitle.innerText = dataTransformed.value; // Affiche la blague
+//         })
+//         // 4. On gère les erreurs (ex: coupure internet)
+//         .catch(error => {
+//             console.error("Erreur lors de l'appel API :", error);
+//         });
+// }
