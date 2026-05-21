2f92955fef25da05e5983b7b1f0367e516131bc0
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-21 13:31:29 +0200

    Ajout d'une section hommage à Chuck Norris avec un bouton pour récupérer une blague aléatoire et amélioration du style des éléments

--- src/exercices/dom-fetch-api.js
+++ src/exercices/dom-fetch-api.js
@@ -1,6 +1,6 @@
 const apiPoke = document.querySelector('.apiPoke');
 
-const apiPokemon =  async () => {
+const apiPokemon = async () => {
     try {
         const rawData = await fetch('https://pokeapi.co/api/v2/pokemon');
         // console.log(rawData);
@@ -15,13 +15,13 @@ const apiPokemon =  async () => {
         // console.log(data.results);
 
         data.results.forEach(pokemon => {
-            // console.log(element.name);
+            // console.log(pokemon.name);
 
             apiPoke.innerHTML += `<li>${pokemon.name}</li>`;
 
             // const liste = document.createElement('li');
             // liste.innerText = pokemon.name;
-            // apiPoke.appendChild(liste);
+            // apiPoke.append(liste);
         });
 
     } catch (error) {
@@ -30,3 +30,26 @@ const apiPokemon =  async () => {
 }
 
 apiPokemon();
+
+const apiChuck = document.querySelector('#chuck');
+
+const apiNorris = async () => {
+    try {
+        const rawData = await fetch('https://api.chucknorris.io/jokes/random');
+        console.log(rawData);
+        if (!rawData.ok || rawData.status !== 200) {
+            console.error(rawData.statusText);
+            return;
+        }
+        const data = await rawData.json();
+        console.log(data);
+        console.log(data.value);
+
+        apiChuck.innerText = data.value;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+document.querySelector('button').addEventListener('click', () => apiNorris());
