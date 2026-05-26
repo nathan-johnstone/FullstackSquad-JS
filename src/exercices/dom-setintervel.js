5b0c2a265323cdd67bac52be39cbdecc432f30f9
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-26 15:07:47 +0200

    Ajout d'un nouvel exercice "DOM setInterval" avec un compte à rebours et un bouton interactif

---
+++ src/exercices/dom-setintervel.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector('#button-exercice-setInterval')
+const timer = document.querySelector('#timer-title')
+
+btn.addEventListener('click', () =>{
+    let counter = 3;
+    const intervalID = setInterval(() => {
+        if(counter > 0){
+            timer.innerHTML = counter;
+            counter = counter - 1;
+        }else{
+            timer.innerHTML = "GO !";
+            clearInterval(intervalID);
+        }}, 1000);
+});
