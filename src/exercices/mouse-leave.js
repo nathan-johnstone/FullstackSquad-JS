7bb553bfda601d3531caaa7edfe548e43d296ffc
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-26 09:22:41 +0200

    écouteur d'événements keyup pour mettre à jour le rendu du texte saisi

--- src/exercices/mouse-leave.js
+++ src/exercices/mouse-leave.js
@@ -1,19 +1,19 @@
-const info = document.querySelector('#gagnant');
-document.addEventListener("mouseleave", () => {
-        info.style.display = "block";
-        info.style.marginTop = "50px";
-        info.style.textAlign = "center";
-        info.style.color = "red";
-        info.style.background = "lime";
-});
-document.addEventListener("mouseover", () => {
-        info.style.display = "none";
-});
-
-// let inputTexte = document.querySelector('input').value;
-// let rendu = document.querySelector('#renderKeyboard');
-// const texte = document.querySelector('input');
-
-// document.addEventListener("keyup", () => {
-//     rendu.innerHTML = inputTexte
+// const info = document.querySelector('#gagnant');
+// document.addEventListener("mouseleave", () => {
+//         info.style.display = "block";
+//         info.style.marginTop = "50px";
+//         info.style.textAlign = "center";
+//         info.style.color = "red";
+//         info.style.background = "lime";
 // });
+// document.addEventListener("mouseover", () => {
+//         info.style.display = "none";
+// });
+
+let inputTexte = document.querySelector('input');
+let rendu = document.querySelector('#renderKeyboard');
+const texte = document.querySelector('input');
+
+document.addEventListener("keyup", () => {
+    rendu.innerHTML = inputTexte.value;
+});
