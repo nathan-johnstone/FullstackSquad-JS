026192f27956f8e5b9069dc8c3cd3c7866170032
nathan-johnstone <nathan.johnstone96@gmail.com>
2026-05-27 16:11:50 +0200

    Réécriture de la logique de stockage local pour pré-remplir le textarea et afficher les données en temps réel

--- src/exercices/web-storage.js
+++ src/exercices/web-storage.js
@@ -1,12 +1,20 @@
-let monTxt = document.querySelector('#note-textarea');
-let renderZone = document.querySelector('.textRender');
+const myTxt = document.querySelector("#note-textarea");
+// console.log(myTxt);
 
-let monSuperTexte = 
+const renderZone = document.querySelector(".textRender");
+// console.log(renderZone);
 
-
-
-
-
-
-
-localStorage.setItem('texte', monTxt.value);
+//On va pré remplir le textarea avec ce que l'on récupère dans le local storage
+myTxt.value = localStorage.getItem('monSuperTexte');
+//Si myTxt.value est définit alors on rempli la Div avec ce qu'on récupère dans le local storage
+// Cela va nous permettre de ne pas perdre les données si on rafraîchit la page
+if(myTxt.value){
+    renderZone.innerText = localStorage.getItem('monSuperTexte');
+};
+//On détecte ce que tape l'utilisateur dans le textarea
+myTxt.addEventListener("keyup", function() {
+//On enregistre ce que tape l'utilisateur dans le keyup sous le nom "monSuperTexte"
+localStorage.setItem('monSuperTexte',myTxt.value);
+//On affiche ce que tape l'utilisateur traduit en marked dans la div     
+    renderZone.innerText =myTxt.value;
+});
